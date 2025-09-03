21b8c37330c5317b014d5c8c4c20a78dbb1b0e50
Diarilala <hei.diarilala@gmail.com>
2025-09-03 12:16:43 +0300

    feat: add exam

--- main.py
+++ main.py
@@ -1,8 +1,52 @@
+import base64
+from typing import List
+from urllib.request import Request
+
 from fastapi import FastAPI
-from starlette.responses import Response
+from pydantic import BaseModel
+from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
 
-@app.get("/hello")
-async def hello():
-    return Response(content="<html><body><h1>Welcome</h1></body></html>", status_code=200, media_type="text/html")
+
+class Characteristic(BaseModel):
+    max_speed: int
+    max_fuel_capacity : int
+
+
+class Car(BaseModel):
+    identifier : str
+    brand : str
+    model : str
+    characteristics : Characteristic
+
+stored_car  = []
+
+
+@app.get("/ping")
+async def ping():
+    return Response(content="pong", status_code=200, media_type="application/json")
+
+
+@app.post("/cars")
+def create_car(car_payload: List[Car]):
+    stored_car.append(car_payload)
+    serialized_car = car_serialized()
+    return Response(content=serialized_car, status_code=200, media_type="text/plain")
+
+def car_serialized():
+    serialized_car = []
+    for c in stored_car:
+        serialized_car.append(c.model_dump())
+    return serialized_car
+
+@app.get("/cars")
+def list_cars():
+    return stored_car
+
+@app.get("/cars/{identifier}")
+def get_car(identifier: int):
+    for i, car in enumerate(stored_car):
+        if car.identifier == identifier:
+            return car[i]
+    return Response(content="Car not found or doesn't exist", status_code=404, media_type="text/plain")
